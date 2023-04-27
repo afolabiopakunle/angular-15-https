1399bd4395ec5aad5346cbdfa5ebca67697b3a17
afolabiopakunle <opakunleafolabi@gmail.com>
2023-04-27 23:07:14 +0100

    paused

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Data } from './data';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -14,11 +15,21 @@ export class AppComponent {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.onFetchPosts();
+  }
 
   onCreatePost(postData: Data) {
     // Send Http request
     this.http.post('https://angular-15s-default-rtdb.firebaseio.com/posts.json', postData)
+    .pipe(map(response => {
+      const postArray = [];
+      for (const key in response) {
+        postArray.push(response[key])
+      }
+      console.log('pA', postArray)
+      return postArray
+    }))
     .subscribe({
       next:(data) => {
         console.log(data);
@@ -28,6 +39,12 @@ export class AppComponent {
 
   onFetchPosts() {
     // Send Http request
+    this.http.get('https://angular-15s-default-rtdb.firebaseio.com/posts.json')
+    .subscribe({
+      next: (response) => {
+        console.log(response)
+      }
+    })
   }
 
   onClearPosts() {
