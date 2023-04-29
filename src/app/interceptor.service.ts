0802283ae59d01bc40eb33a8b388fd313ce55563
afolabiopakunle <opakunleafolabi@gmail.com>
2023-04-29 13:13:14 +0100

    done

--- src/app/interceptor.service.ts
+++ src/app/interceptor.service.ts
@@ -4,8 +4,11 @@ import { HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http"
 export class InterceptorService implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
+   const sureQ = req.clone({
+      headers: req.headers.append('auth-me', 'authed nows')
+    })
     console.log('Request going...')
-    return next.handle(req)
+    return next.handle(sureQ)
   }
 
 }
